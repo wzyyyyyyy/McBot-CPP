3dc90ef10c0f57fb27765db6554090e5827c42b7
Administrator <Administrator@HECS-X-XLARGE-2>
2020-12-16 23:58:06 +0800

    更新咯

--- McBot/源.cpp
+++ McBot/源.cpp
@@ -16,10 +16,15 @@
 #endif
 SOCKET client;
 std::list<std::string> playerlist;
+std::list<void*> playerptr;
+uintptr_t _cmdqueue;
 bool client_first = true;
 void initialization();
 bool s_send(std::string msg);
 void CreatSocket();
+void runcmd(std::string cmd);
+bool isconnect = true;
+bool run = false;
 std::string getOnlinePlayer();
 template <size_t size> void UtoA_Fill(char(&buf)[size], int num) {
 	int nt = size - 1;
@@ -49,6 +54,19 @@ string getTime() {
 	str += buf;
 	return "["+str+" BOT]";
 }
+THook(void*, "??0?$SPSCQueue@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@$0CAA@@@QEAA@_K@Z",
+	uintptr_t _this) {
+	_cmdqueue = (uintptr_t)original(_this);
+	return (void*)_cmdqueue;
+}
+THook(uintptr_t, "??$_Insert_string@DU?$char_traits@D@std@@_K@std@@YAAEAV?$basic_ostream@DU?$char_traits@D@std@@@0@AEAV10@QEBD_K@Z",
+	uintptr_t handle, const char* str, uintptr_t size) {
+	if (run) {
+		run = 0;
+		s_send(u8"{\"type\":\"logout\",\"msg\":\""+(string)str+"\"}");
+	}
+	return original(handle, str, size);
+}
 THook(void*,"?createChat@TextPacket@@SA?AV1@AEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@000@Z", void* _this, std::string& player_name, std::string& target, std::string& msg, std::string& chat_style) {
 	string msg_ = u8"{\"type\":\"ChatEvent\",\"PlayerName\":\"" + player_name + "\",\"msg\":\"" + target + "\"}";
 	s_send(msg_);
@@ -56,22 +74,25 @@ THook(void*,"?createChat@TextPacket@@SA?AV1@AEBV?$basic_string@DU?$char_traits@D
 }
 THook(void, "?onPlayerJoined@ServerScoreboard@@UEAAXAEBVPlayer@@@Z",
 	void* a1, Player* p) {
+	playerptr.push_back(p);
 	playerlist.push_back(p->getNameTag());
+	std::cout << p << std::endl;
 	string msg = u8"{\"type\":\"PlayerJoinEvent\",\"PlayerName\":\"" + p->getNameTag() + "\",\"onlinePlayer\":\"" + getOnlinePlayer() + "\"}"; 
 	s_send(msg);
 	return original(a1, p);
 }
 THook(void, "?die@Mob@@UEAAXAEBVActorDamageSource@@@Z",
 	Mob* mob, void* a1) {
-	int mobid = access(mob, int, 948); //IDA Actor::getEntityType
-	if (mobid == 1 && mob->getNameTag() != "") {
-		string msg = u8"{\"type\":\"PlayerDieEvent\",\"PlayerName\":"+mob->getNameTag()+"\",\"pos\":\"" + std::to_string((int)mob->getPos().x) + ";" + std::to_string((int)mob->getPos().y) + ";" + std::to_string((int)mob->getPos().z) + "\"}";
+	int mobid = (*(int*)(((uintptr_t)mob) + 964));//IDA Actor::getEntityType
+	if (mobid == 1 && std::count(playerptr.begin(),playerptr.end(),mob)) {
+		string msg = u8"{\"type\":\"PlayerDieEvent\",\"PlayerName\":\"" + mob->getNameTag() + "\",\"pos\":\"" + std::to_string((int)mob->getPos().x) + ";" + std::to_string((int)mob->getPos().y) + ";" + std::to_string((int)mob->getPos().z) + "\"}";
 		s_send(msg);
 	}
 	return original(mob, a1);
 }
 THook(void, "?_onPlayerLeft@ServerNetworkHandler@@AEAAXPEAVServerPlayer@@_N@Z",
 	void* _this, Player* p, char v3) {
+	playerptr.remove(p);
 	playerlist.remove((p->getNameTag()));
 	string msg_ = u8"{\"type\":\"PlayerLeftEvent\",\"PlayerName\":\"" + p->getNameTag() + "\",\"onlinePlayer\":\""+getOnlinePlayer()+"\"}";
 	s_send(msg_);
@@ -82,22 +103,42 @@ void entry() {
 	std::cout << getTime() << " [McBot] Loaded!\n";
 }
 void CreatSocket() {
-	SOCKADDR_IN server;
-	if (client_first) {
-		initialization();
-		client_first = false;
-	}
-	server.sin_family = AF_INET;
-	server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-	server.sin_port = htons(61046);
-	client = socket(AF_INET, SOCK_STREAM, 0);
-	if (connect(client, (SOCKADDR*)&server, sizeof(SOCKADDR)) == SOCKET_ERROR) {
-		cout << getTime() << u8" Socket连接失败！" << endl;
-		WSACleanup();
-	}
-	else {
-		cout << getTime() << u8" Socket连接成功！" << endl;
-	}
+	std::thread t([]() {
+		SOCKADDR_IN server;
+		if (client_first) {
+			initialization();
+			client_first = false;
+		}
+		server.sin_family = AF_INET;
+		server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+		server.sin_port = htons(61046);
+		client = socket(AF_INET, SOCK_STREAM, 0);
+		if (connect(client, (SOCKADDR*)&server, sizeof(SOCKADDR)) == SOCKET_ERROR) {
+			cout << getTime() << u8" Socket连接失败！" << endl;
+			WSACleanup();
+		}
+		else {
+			cout << getTime() << u8" Socket连接成功！" << endl;
+			isconnect = 1;
+			while (isconnect) {
+				char buffer[0xff] = { 0 };
+				recv(client, buffer, 0xff, NULL);
+				rapidjson::Document json;
+				json.Parse(buffer);
+				std::string type = json["type"].GetString();
+				if (type == "runcmd") 
+				{
+					run = json["run"].GetBool();
+					runcmd(json["cmd"].GetString());
+				}
+			}
+		}
+		});
+	t.detach();
+}
+void runcmd(std::string cmd) {
+	SymCall("??$inner_enqueue@$0A@AEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@?$SPSCQueue@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@$0CAA@@@AEAA_NAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@Z",
+		bool, uintptr_t, std::string)(_cmdqueue, cmd);
 }
 bool s_send(std::string msg) {
 	int i = send(client, msg.c_str(), msg.length(), 0);
@@ -105,12 +146,15 @@ bool s_send(std::string msg) {
 		std::cout << getTime() << u8" Socket连接已断开！正在尝试重连......" << endl;
 		CreatSocket();
 		send(client, msg.c_str(), msg.length(), 0);
+		isconnect = 0;
 		return 0;
 	}
 	else if (GetLastError()) {
 		std::cout << getTime() << u8" Socket连接已断开！正在尝试重连......" << endl;
 		CreatSocket();
 		send(client, msg.c_str(), msg.length(), 0);
+		isconnect = 0;
+		return 0;
 	}
 	return 1;
 }
